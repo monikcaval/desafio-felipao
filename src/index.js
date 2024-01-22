9fda798d2b09e8efbffa5256dc64c61b496a1098
Monik <152573131+monikcaval@users.noreply.github.com>
2024-01-22 15:38:07 -0300

    desafio felipao dio
    entregando o desafio dio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-let nome = "felipe";
-console.log(nome);
+let nomeHeroi = "Zeldinha";
+let xpHeroi = 450
+let mensagem;
+
+if (xpHeroi < 1000){
+  mensagem = "Zeldinha, esta dando as boas vindas!";
+} else {
+  mensagem = "Zeldinha é o super heroi favotiro das crianças"
+}
+console.log(mensagem);
